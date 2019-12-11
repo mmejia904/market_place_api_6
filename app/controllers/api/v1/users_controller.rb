f8ca8e1b1408b5c226e1354aa1d5adcc69f43b05
mmejia904 <mmejia904@gmail.com>
2019-12-11 18:25:36 -0500

    Tests before creating users

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,5 @@
-class Api::V1::UsersController < ApplicationController
-    # GET /users/1
+class Api::V1::UsersController < ApplicationController # GET /users/1
     def show
-        render json: User.find(params[:id])
+        render json: User.find(params[:id]) 
     end
-
 end
