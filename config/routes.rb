058f30fadf62bb78f05aa6af8c4b42df3959f693
mmejia904 <mmejia904@gmail.com>
2019-12-11 16:29:35 -0500

    Create user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
   #API definition
-  namespace :api, defaults: {format: :json} do
-    namespace: :v1 do
-      #We are going to list our resources here
-      
-    
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      #We are going to list our resources here 
     end
   end
 end
