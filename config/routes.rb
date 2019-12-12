d3e5e171ea06113b932638876695430ca61c57fb
mmejia904 <mmejia904@gmail.com>
2019-12-12 12:37:44 -0500

    Adds the user create endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   #API definition
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :users, only: [:show]
+      resources :users, only: %i[show create]
       #We are going to list our resources here 
     end
   end
--- test/controllers/api/v1/users_controller_test.rb
+++ test/controllers/api/v1/users_controller_test.rb
@@ -14,4 +14,21 @@ class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
     assert_equal @user.email, json_response['email']
   end
 
+  #Test to check creation of a user by sending a valid POST request. Then, we check that the additional
+  #user exists in the database and the HTTP code response is created (201)
+  test "should create user" do
+    assert_difference('User.count') do
+      post api_v1_users_url, params: { user: { email: 'test@test.org', password: '123456' } }, as: :json
+    end
+    assert_response :created
+  end
+
+  #Test to check that the user is not created using that email already. Then, we check that the code of
+  #response is unprocessable_entity (422)
+  test "should not create user with taken email" do
+    assert_no_difference('User.count') do
+      post api_v1_users_url, params: { user: { email: @user.email, password: '123456' } }, as: :json
+    end
+    assert_response :unprocessable_entity
+  end
 end
