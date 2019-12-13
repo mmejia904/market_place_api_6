6ed854104fb41cada18ad67b5e49ba43842a18ae
mmejia904 <mmejia904@gmail.com>
2019-12-13 15:07:53 -0500

    Setup JWT gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,20 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      get 'tokens/create'
+    end
+  end
   #API definition
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :users, only: %i[show create update destroy]
       #We are going to list our resources here 
+
+      resources :tokens, only: [:create]
     end
   end
 end
+
+# Te desired behavior is the following:
+# I recieve a token if I send a valid email/password pair
+# Otherwise server respond a forbidden response
