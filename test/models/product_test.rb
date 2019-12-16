0c6339802a20522f1c298f7a3845e2ced5c78562
mmejia904 <mmejia904@gmail.com>
2019-12-16 14:20:27 -0500

    Adds some validations to products

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,6 +1,12 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
+  test "Should have a positive price" do
+    product = products(:one)
+    product.price = -1
+    assert_not product.valid?
+  end
+
   # test "the truth" do
   #   assert true
   # end
