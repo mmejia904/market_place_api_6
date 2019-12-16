9ddeccce06905ba73bc9198f8f7283ad584de7d1
mmejia904 <mmejia904@gmail.com>
2019-12-16 10:06:02 -0500

    Setup tokens controller

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,30 @@ Rails.application.routes.draw do
   end
 end
 
+class Api::V1::TokensController < ApplicationController
+  def create
+    @user = User.find_by_email(user_params[:email])
+    if @user&.authenticate(user_params[:password])
+      render json: {
+        token: JsonWebToken.encode(user_id: @user.id), 
+        email: @user.email
+      }
+    else
+      head :unauthorized
+    end
+  end
+
+  private
+
+  # Only allow a trusted parameter "white list" through.
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
+end
+
+
+
+
 # Te desired behavior is the following:
 # I recieve a token if I send a valid email/password pair
 # Otherwise server respond a forbidden response
