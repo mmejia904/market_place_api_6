b42ccc110d4fc45ddd140d3925532764df3da446
mmejia904 <mmejia904@gmail.com>
2019-12-17 14:45:28 -0500

    Adds the products create, update and destroy actions

--- config/routes.rb
+++ config/routes.rb
@@ -10,33 +10,34 @@ Rails.application.routes.draw do
       resources :users, only: %i[show create update destroy]
       resources :tokens, only: [:create]
       resources :products, only: %i[show index]
+
       #We are going to list our resources here 
-
       resources :tokens, only: [:create]
+      resources :products
     end
   end
 end
 
-class Api::V1::TokensController < ApplicationController
-  def create
-    @user = User.find_by_email(user_params[:email])
-    if @user&.authenticate(user_params[:password])
-      render json: {
-        token: JsonWebToken.encode(user_id: @user.id), 
-        email: @user.email
-      }
-    else
-      head :unauthorized
-    end
-  end
+# class Api::V1::TokensController < ApplicationController
+#   def create
+#     @user = User.find_by_email(user_params[:email])
+#     if @user&.authenticate(user_params[:password])
+#       render json: {
+#         token: JsonWebToken.encode(user_id: @user.id), 
+#         email: @user.email
+#       }
+#     else
+#       head :unauthorized
+#     end
+#   end
 
-  private
+#   private
 
-  # Only allow a trusted parameter "white list" through.
-  def user_params
-    params.require(:user).permit(:email, :password)
-  end
-end
+#   # Only allow a trusted parameter "white list" through.
+#   def user_params
+#     params.require(:user).permit(:email, :password)
+#   end
+# end
 
 
 
